50092dbd4a1d7d30473e25355056af4973fc5f50
manikanthch2779 <manikanthch2779@gmail.com>
2026-07-08 20:19:01 +0930

    Added Circumference of circle program

---
+++ Basic Programs/Circumference_of_circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float rad,cir;
+    printf("Enter the radius of the circle: ");
+    scanf("%f",&rad);
+    cir=2*3.14*rad;
+    printf("Circumference of the circle is %f",cir);
+    return 0;
+}
