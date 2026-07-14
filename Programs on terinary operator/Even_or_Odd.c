c7384908ad22c0763eac3ea63fa70279d9a31fda
manikanthch2779 <manikanthch2779@gmail.com>
2026-07-14 18:38:51 +0930

    Added Even or Odd no. finder

---
+++ Programs on terinary operator/Even_or_Odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0)
+        printf("\n%d is even.", num);
+    else
+        printf("\n%d is odd.", num);
+    return 0;
+}
